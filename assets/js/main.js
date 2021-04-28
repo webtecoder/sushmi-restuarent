9fe08dc929e83af34565186f6291a7455af373d0
sazzadhossen360 <sazzadhossen2580@gmail.com>
2021-04-28 09:22:17 +0600

    added off canvas overlay and scroll top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,38 @@
-(function ($) {
-	"use strict";
+// (function ($) {
+// 	"use strict";
 
-    jQuery(document).ready(function($){
-        
-    });
     
-    jQuery(window).load(function(){
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "100%";
+            document.getElementById("main").style.marginLeft = "250px";
+          }
+          
+          function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft= "0";
+          }
+
+
+          
+  // scroll to top js
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 200){
+      $('#scroll').addClass('scroll-show');
+    }
+    else{
+      $('#scroll').removeClass('scroll-show');
+    }
+  })
+
+  $('#scroll').click(function(){
+    $('html, body').animate({
+      scrollTop: 0
+    },500)
+  
+  });
+   
+    
+//     jQuery(window).load(function(){
         
-    });
-}(jQuery));	
+//     });
+// }(jQuery));	
